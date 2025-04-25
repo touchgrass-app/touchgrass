f48635cfec90365ad36a50496743042c238630ef
chaitany Goyal <chirtgeryel57@gmail.com>
2025-04-25 21:59:08 +0800

    Made the following changes: - ndkVersion to be choose latest - fixed outdated reference in home_screen.dart - unreferenced .env file. Cause it doesn't exist >m<

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.touchgrass"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+//    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
