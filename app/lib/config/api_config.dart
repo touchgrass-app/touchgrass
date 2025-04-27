a03b72beaf54e2160e1b7b449c4628e568431f1f
Dhruv Jobanputra <dhruvj@canva.com>
2025-04-27 20:48:06 +0800

    using env for api url, remove unused ndk version

--- app/lib/config/api_config.dart
+++ app/lib/config/api_config.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConfig {
-  static String get baseUrl => 'http://localhost:8080/api';
+  static String get baseUrl => '${dotenv.env['API_BASE_URL']}/api';
 
   static String get login => '$baseUrl/auth/login';
   static String get register => '$baseUrl/auth/register';
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
 import 'screens/home_screen.dart';
 import 'models/user.dart';
 import 'services/auth_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
